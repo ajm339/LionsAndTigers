90545c1a92f5e357c2c25a7ee2b6547fc3fa62a6
Alex Meyers <ajm339@cornell.edu>
2015-01-10 16:34:16 -0500

    through instantiating first struct

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Alex Meyers on 1/8/15.
+//  Copyright (c) 2015 alex. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger{
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+}
